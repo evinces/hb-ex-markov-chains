6a339e618e5ed605cc540e23895ec139a22a2311
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-13 16:45:40 -0700

    reimplement make_chains and make_text without try except

--- markov.py
+++ markov.py
@@ -15,6 +15,72 @@ def open_and_read_file(file_path):
     return open(file_path).read()
 
 
+# def make_chains(text_string, n_gram=2):
+#     """Take input text as string; return dictionary of Markov chains.
+
+#     A chain will be a key that consists of a tuple of (word1, word2)
+#     and the value would be a list of the word(s) that follow those two
+#     words in the input text.
+
+#     For example:
+
+#         >>> chains = make_chains("hi there mary hi there juanita")
+
+#     Each bigram (except the last) will be a key in chains:
+
+#         >>> sorted(chains.keys())
+#         [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+
+#     Each item in chains is a list of all possible following words:
+
+#         >>> chains[('hi', 'there')]
+#         ['mary', 'juanita']
+
+#         >>> chains[('there','juanita')]
+#         [None]
+#     """
+
+#     chains = {}
+#     words = text_string.split()
+#     for i in range(len(words) - n_gram + 1):
+#         key_list = []
+#         for j in range(n_gram):
+#             key_list.append(words[i+j])
+#         key_tuple = tuple(key_list)
+
+#         try:
+#             if key_tuple in chains:
+#                 chains[key_tuple].append(words[i+n_gram])
+#             else:
+#                 chains[key_tuple] = [words[i+n_gram]]
+#         except IndexError:
+#             chains[key_tuple] = [None]
+
+#     return chains
+
+
+# def make_text(chains):
+#     """Return text from chains."""
+
+#     words = []
+#     link = choice([key for key in chains.keys() if key[0][0].isupper()])
+#     words.extend(link)
+
+#     next_word = choice(chains[link])
+#     while next_word is not None:
+#         words.append(next_word)
+
+#         link_list = []
+#         for i in range(1, len(link)):
+#             link_list.append(link[i])
+#         link_list.append(next_word)
+#         link = tuple(link_list)
+
+#         next_word = choice(chains[link])
+
+#     return " ".join(words)
+
+
 def make_chains(text_string, n_gram=2):
     """Take input text as string; return dictionary of Markov chains.
 
@@ -42,19 +108,16 @@ def make_chains(text_string, n_gram=2):
 
     chains = {}
     words = text_string.split()
-    for i in range(len(words) - n_gram + 1):
+    for i in range(len(words) - n_gram):
         key_list = []
         for j in range(n_gram):
             key_list.append(words[i+j])
         key_tuple = tuple(key_list)
 
-        try:
-            if key_tuple in chains:
-                chains[key_tuple].append(words[i+n_gram])
-            else:
-                chains[key_tuple] = [words[i+n_gram]]
-        except IndexError:
-            chains[key_tuple] = [None]
+        if key_tuple in chains:
+            chains[key_tuple].append(words[i+n_gram])
+        else:
+            chains[key_tuple] = [words[i+n_gram]]
 
     return chains
 
@@ -66,30 +129,34 @@ def make_text(chains):
     link = choice([key for key in chains.keys() if key[0][0].isupper()])
     words.extend(link)
 
-    next_word = choice(chains[link])
-    while next_word is not None:
-        words.append(next_word)
+    while True:
+        if chains.get(link) is not None:
+            next_word = choice(chains[link])
+            words.append(next_word)
 
-        link_list = []
-        for i in range(1, len(link)):
-            link_list.append(link[i])
-        link_list.append(next_word)
-        link = tuple(link_list)
-
-        next_word = choice(chains[link])
+            link_list = []
+            for i in range(1, len(link)):
+                link_list.append(link[i])
+            link_list.append(next_word)
+            link = tuple(link_list)
+        else:
+            break
 
     return " ".join(words)
 
 
 def get_file_path():
-    """Check path arguments for filename, else use gettysburg.txt"""
+    """Check path arguments for filename, else use green-eggs.txt"""
 
     if len(argv) > 1:
         if isfile(argv[1]):
             return argv[1]
         else:
-            print "{} is not a file. Using gettysburg.txt as default.".format(argv[1])
-    return "gettysburg.txt"
+            print "{} is not a file.".format(argv[1])
+            print "Using green-eggs.txt as default."
+
+    return "green-eggs.txt"
+    # return "gettysburg.txt"
 
 
 input_path = get_file_path()
@@ -99,12 +166,6 @@ input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
 chains = make_chains(input_text)
-
-# input_path = "green-eggs.txt"
-# input_text = open_and_read_file(input_path)
-# add_to_chains = make_chains(input_text)
-
-# chains.update(add_to_chains)
 
 # Produce random text
 random_text = make_text(chains)
