8097585deb778958349eeb31d9da401a6e5f0400
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-13 13:09:49 -0700

    completed exercise + further study

--- markov.py
+++ markov.py
@@ -3,6 +3,7 @@
 from random import choice
 from sys import argv
 
+
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
 
@@ -10,13 +11,10 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
-    file_string = open(file_path).read()
-
-    return file_string
+    return open(file_path).read()
 
 
-def make_chains(text_string):
+def make_chains(text_string, n_gram=2):
     """Take input text as string; return dictionary of Markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -41,14 +39,18 @@ def make_chains(text_string):
         [None]
     """
     chains = {}
-    lst = text_string.split()
-    for i in range(len(lst)-1):
-        key_tuple = (lst[i], lst[i+1])
+    words = text_string.split()
+    for i in range(len(lst) - n_gram + 1):
+        key_list = []
+        for j in range(n_gram):
+            key_list.append(lst[i+j])
+        key_tuple = tuple(key_list)
+
         try:
             if key_tuple in chains:
-                chains[key_tuple].append(lst[i+2])
+                chains[key_tuple].append(lst[i+n_gram])
             else:
-                chains[key_tuple] = [lst[i+2]]
+                chains[key_tuple] = [lst[i+n_gram]]
         except IndexError:
             chains[key_tuple] = [None]
 
@@ -60,29 +62,42 @@ def make_text(chains):
 
     words = []
     keys = chains.keys()
+    keys = [key for key in keys if key[0][0].isupper()]
     link = choice(keys)
     words.extend(link)
+
     next_word = choice(chains[link])
     while next_word is not None:
         words.append(next_word)
-        link = (link[1], next_word)
+
+        link_list = []
+        for i in range(1, len(link)):
+            link_list.append(link[i])
+        link_list.append(next_word)
+        link = tuple(link_list)
+
         next_word = choice(chains[link])
 
     return " ".join(words)
 
 
-# input_path = "green-eggs.txt"
-#input_path = "gettysburg.txt"
 if len(argv) > 1:
     input_path = argv[2]
 else:
+    # input_path = "green-eggs.txt"
     input_path = "gettysburg.txt"
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text, 3)
+chains = make_chains(input_text)
+
+input_path = "green-eggs.txt"
+input_text = open_and_read_file(input_path)
+add_to_chains = make_chains(input_text)
+
+chains.update(add_to_chains)
 
 # Produce random text
 random_text = make_text(chains)
